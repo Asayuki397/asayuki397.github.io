539da9a60614897aa4eac9e07243283ee463cdf2
asayuki397 <lunarari0301@gmail.com>
2025-04-25 17:39:40 +0900

    run prettier

--- src/portfolio.js
+++ src/portfolio.js
@@ -26,7 +26,7 @@ const greeting = {
     "A Growing Junior Software Developer 🚀 having an experience of building games / applications with Unity / GameMaker and some other cool libraries and frameworks."
   ),
   //resumeLink: "https://docs.google.com/document/d/1S2cwcxMEEFjqNX7kzwXAICQF8S_YrHrkCdFkvvzjHGM/edit?usp=sharing",
-     // Set to empty to hide the button
+  // Set to empty to hide the button
   displayGreeting: true // Set false to hide this section, defaults to true
 };
 
